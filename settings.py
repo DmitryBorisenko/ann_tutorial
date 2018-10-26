ab6f2af9d5c0ab13aaa76df5aa3f1c63a8daf3b7
Dmitry Borisenko <dmitrij.borisenko@gmail.com>
2018-10-27 00:09:25 +0200

    these settings do not belon in here

--- settings.py
+++
@@ -1,6 +0,0 @@
-settings = {
-    "fx_data_path": "C:/Users/borisenko/Documents/ann_external_data/fx_data/",
-    "rx_daily_file": "daily_rx.p",
-    "sample_start": "2000-11-01",
-    "sample_end": "2017-03-31",
-    }
